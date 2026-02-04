54701d3afa22b6d39ffaf5a9144ddd8c40bcf740
LTasaycoQ <luistasayco3030@gmail.com>
2026-02-04 15:33:37 -0500

    correccion - reubicacion-lista data room includes

---
+++ src/content/roomsInclude.js
@@ -0,0 +1,44 @@
+
+export const iconsRoomIncludes = [
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/ducha-rggra34qypmfdprvxwhvjyghee0t9mldtvcmdtb12s.webp?w=800&ssl=1",
+        label: "Baño privado",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/ducha-rggra34qypmfdprvxwhvjyghee0t9mldtvcmdtb12s.webp?w=800&ssl=1",
+        label: "Baño privado",
+    },
+
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/caliente-rggr9o3bxd1u7ydqdpzug293w82xugxofswupdxbuc.png?w=800&ssl=1",
+        label: "Caliente",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/tv-rggrb1wrzky1baddatj2m7ojdn9e6ffganhl51vgok.webp?w=800&ssl=1",
+        label: "TV",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/wifi-rggrb3sgd90lyiamzucbr77gkf04ltmwywsk3lsoc4.webp?w=800&ssl=1",
+        label: "WiFi",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/despertar-rggra0b8e7ikevvzed9zuh63m8epmja6the5xzf7lg.webp?w=800&ssl=1",
+        label: "Despertador",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/secadora-rggrax7l1erlp8k729hxrqv8epwk3xwsm085qo2fjo.webp?w=800&ssl=1",
+        label: "Caliente",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/secadora-rggrax7l1erlp8k729hxrqv8epwk3xwsm085qo2fjo.webp?w=800&ssl=1",
+        label: "Caliente",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/secadora-rggrax7l1erlp8k729hxrqv8epwk3xwsm085qo2fjo.webp?w=800&ssl=1",
+        label: "Caliente",
+    },
+    {
+        image: "https://i0.wp.com/terraandinahotel.com/wp-content/uploads/elementor/thumbs/secadora-rggrax7l1erlp8k729hxrqv8epwk3xwsm085qo2fjo.webp?w=800&ssl=1",
+        label: "Caliente",
+    },
+];
